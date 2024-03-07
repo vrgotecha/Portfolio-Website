f6933e8d11225aba824803f0211a707a4eb15bb4
Vivek Gotecha <vivekrgotecha@gmail.com>
2024-03-07 06:21:14 +0530

    edited HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
-import { FiDribbble } from "react-icons/fi";
+import { SiLeetcode } from "react-icons/si";
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/vivek-gotecha-4360391ba/" target="_blank" rel="noreferrer"> <BsLinkedin /> </a>
         <a href="https://github.com/vrgotecha" target="_blank" rel="noreferrer"> <FaGithub /> </a>
-        <a href="https://dribbble.com/vivekrg" target="_blank" rel="noreferrer"> <FiDribbble /> </a>
+        <a href="https://leetcode.com/coder-vivek_03/" target="_blank" rel="noreferrer"> <SiLeetcode /> </a>
         {/* <a href="https://linkedin.com" target="_blank"></a> */}
     </div>
   )
