031fb507f1c1ce92e6f5e55abdd47ccb43c4671e
Vivek Gotecha <vivekrgotecha@gmail.com>
2024-03-12 20:05:16 +0530

    changed Experience.jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -92,7 +92,7 @@ const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className='experience__details-icon'/>
               <div>
-                <h4>Python</h4>
+                <h4>Django</h4>
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
