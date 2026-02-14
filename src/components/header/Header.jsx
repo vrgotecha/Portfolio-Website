4ffa4bf6d50eb2cea31b82f664621e39b5f59140
Vivek Gotecha <vivekrgotecha@gmail.com>
2026-02-14 18:23:00 +0530

    changes made in Header.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
      
 
         <div className="me">
-          <img src={ME} alt="Vivek Gotecha profile" />
+          <img src={ME} alt="me" />
         </div>
 
         <a href="#contact" className='scroll__down'>Scroll down</a>
