5f02d78b43f6acf5c1bc22c8c0131a9525e9a33f
Vivek Gotecha <vivekrgotecha@gmail.com>
2026-02-16 12:45:19 +0530

    Changed Header.jsx and header.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/Vivek_Gotecha.jpg'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
@@ -13,10 +12,19 @@ const Header = () => {
         <h5 className="text-light">Fullstack developer crafting clean, scalable digital products</h5>
         <CTA />
         <HeaderSocials/>
-     
-
-        <div className="me">
-          <img src={ME} alt="me" />
+        <div className="header__highlights">
+          <article>
+            <h6>3+ Years</h6>
+            <small>Experience</small>
+          </article>
+          <article>
+            <h6>20+ Projects</h6>
+            <small>Built and shipped</small>
+          </article>
+          <article>
+            <h6>Fullstack</h6>
+            <small>React, Node, and cloud</small>
+          </article>
         </div>
 
         <a href="#contact" className='scroll__down'>Scroll down</a>
